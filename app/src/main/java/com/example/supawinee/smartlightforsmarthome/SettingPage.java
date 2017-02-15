5b6726dd09a96ab6c0688a49162d1e7eedecdf17
Supawinee Maknuan <supawinee.mak@gmail.com>
2017-02-15 17:32:41 +0700

    Edit Some Setting page

--- app/src/main/java/com/example/supawinee/smartlightforsmarthome/SettingPage.java
+++ app/src/main/java/com/example/supawinee/smartlightforsmarthome/SettingPage.java
@@ -1,13 +1,46 @@
 package com.example.supawinee.smartlightforsmarthome;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class SettingPage extends AppCompatActivity {
+
+    final String P_NAME = "App_Setting";
+
+    EditText appID_SettingEditText;
+    EditText key_SettingEditText;
+    EditText secret_SettingEditText;
+    EditText alias_SettingEditText;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting_page);
+
+
+
+
+
+        ////////////////////////// FOR SHAREDPREFERENCE /////////////
+        appID_SettingEditText = (EditText)findViewById(R.id.appid_EditText);
+        key_SettingEditText = (EditText)findViewById(R.id.key_EditText);
+        secret_SettingEditText = (EditText)findViewById(R.id.secret_EditText);
+        alias_SettingEditText = (EditText)findViewById(R.id.alias_EditText);
+
+
+        SharedPreferences sp = getSharedPreferences(P_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit(); 
+        editor.putBoolean("FirstRun", true);
+        editor.commit();
+
+
     }
 }
